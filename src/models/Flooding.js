add1902d6270e11752fc87448d2bfc93668da6eb
Bruno Fernandes <brunofdsleite@gmail.com>
2021-02-07 11:10:23 -0300

    changed model

--- src/models/Flooding.js
+++ src/models/Flooding.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let Flooding = new Schema({
-  name: {type: String},
   latitude: {type: Number},
   longitude: {type: Number},
-  note: {type: Number},
-  createAt: {type: String},
+  note: {type: String},
+  createAt: { type : Date, default: Date.now },
   user: {type: String}
 });
 
