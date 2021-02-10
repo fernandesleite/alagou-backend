a23364c1d9509ade5fe3377d6ec0a8387c4b5f33
Bruno Fernandes <brunofdsleite@gmail.com>
2021-02-10 09:23:53 -0300

    Deploying to Heroku-AtlasDb

--- src/server.js
+++ src/server.js
@@ -5,17 +5,16 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
-const url = "mongodb://localhost/myapp";
-let mongoDB = process.env.MONGODB_URI || url;
-mongoose.connect(mongoDB,{useNewUrlParser: true, useUnifiedTopology: true });
+let mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
-db.on("error", console.error.bind(console, "Erro na Ligação ao MongoDB"));
-db.on("open", console.error.bind(console, "connected"));
+db.on("error", console.error.bind(console, "Connection to Database Failed"));
+db.on("open", console.error.bind(console, "Connection to Database was successfully established"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', routes);
 
-app.listen(8888,  () => {
+app.listen(process.env.PORT || 3000,  () => {
   console.log("Servidor em execução")});
